65dbadba1d82f80fe48288b18fb196e54a562788
onowdev <onowdev@gmail.com>
2022-07-07 13:09:31 +0700

    aturan Penamaan

--- Basic/2Variabel_1.py
+++ Basic/2Variabel_1.py
@@ -6,4 +6,32 @@ There are several techniques you can use to make them more readable:
 Aturan Penamaan Variabel yang lebih dari 1 Kosakata
 Nama Variabel dengan 1 atau lebih kata akan sulit dibaca 
 
+Berikut bebarapa aturan penamaan variabel, agar lebih mudah dibaca
+
+1. Aturan CamelCase
+Each word, except the first, starts with a capital letter:
+
+cth : myVariableName = "Jonny"
+
+2. Aturan Pascal Case
+Each word starts with a capital letter:
+
+cth : MyVariableName = "Jonny"
+
+3. Snake Case
+Each word is separated by an underscore character:
+
+cth: my_variable_name = "Jonny"
 """
+# Aturan CamelCase
+
+myVariableName = "Agus"
+print("camelCase:", myVariableName)
+
+# Aturan Pascal
+MyVariableName = "Andra"
+print("PascalCase: ", MyVariableName)
+
+# Aturan Snake
+my_variable_name = "Devy_Andra"
+print("Snake_case: ", my_variable_name)
