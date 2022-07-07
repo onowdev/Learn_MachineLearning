d6682d56a07b8900a9f6ce5b3a58c95f8fafec68
onowdev <onowdev@gmail.com>
2022-07-07 11:50:19 +0700

    variabel V2

---
+++ Basic/2Variabel_1.py
@@ -0,0 +1,9 @@
+"""
+Multi Words Variable Names
+Variable names with more than one word can be difficult to read.
+There are several techniques you can use to make them more readable:
+
+Aturan Penamaan Variabel yang lebih dari 1 Kosakata
+Nama Variabel dengan 1 atau lebih kata akan sulit dibaca 
+
+"""
