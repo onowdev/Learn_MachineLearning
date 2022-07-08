07b40ddfc67229af6dfa07f88140da0adf97cc51
onowdev <onowdev@gmail.com>
2022-07-08 11:29:09 +0700

    tipeData Lanjutan

---
+++ Basic/3TipeDataLanjutan.py
@@ -0,0 +1,21 @@
+"""Python Data Types
+Built-in Data Types
+In programming, data type is an important concept.
+
+Variables can store data of different types, and different types can do different things.
+
+Python has the following data types built-in by default, in these categories:
+
+Text Type       :	str
+Numeric Types   :	int, float, complex
+Sequence Types  :	list, tuple, range
+Mapping Type    :	dict
+Set Types       :	set, frozenset
+Boolean Type    :	bool
+Binary Types    :	bytes, bytearray, memoryview
+None Type       :	NoneType
+"""
+# example
+
+x = 5
+print(type(x))
