b5b698a6c5d72a7ebeb8a7b99d4b5b27c5454605
onowdev <onowdev@gmail.com>
2022-07-10 22:26:53 +0700

    add description

---
+++ Basic/5Numbers.py
@@ -0,0 +1,24 @@
+"""
+Python Numbers
+There are three numeric types in Python:
+
+1. int >> Int or integer, is a whole number, 
+positive or negative, without decimals, 
+of unlimited length.
+
+2. float >> Float or "floating point number" 
+is a number, positive or negative, containing 
+one or more decimals.
+
+3. complex >> Complex
+Complex numbers are written 
+with a "j" as the imaginary part:
+"""
+
+x = 1
+y = 9.9
+z = 9j
+
+print(type(x))
+print(type(y))
+print(type(z))
