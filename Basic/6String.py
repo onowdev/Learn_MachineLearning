9467b944d52627f8409dba1ebd68625845be4221
onowdev <onowdev@gmail.com>
2022-07-11 09:47:57 +0700

    string101

---
+++ Basic/6String.py
@@ -0,0 +1,30 @@
+"""
+Strings
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function:
+"""
+print("Hello_World!")
+#same AS
+print('Hello_world!')
+
+#Assign String to a Variable Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+
+a = "Makan_Pagi"
+print(a) # mencetak Variabel a ke Layar
+
+# Multiline Strings You can assign a multiline string to a variable by using three quotes:
+
+b = """ 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+deserunt mollit anim id est laborum.
+"""
+
+print(b)
