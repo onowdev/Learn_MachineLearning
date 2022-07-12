24dc0e5522a7796dddc691753d7f13ee0bfdd925
onowdev <onowdev@gmail.com>
2022-07-12 21:24:37 +0700

    String Array

--- Basic/6String.py
+++ Basic/6String.py
@@ -28,3 +28,14 @@ deserunt mollit anim id est laborum.
 """
 
 print(b)
+
+"""
+Strings are Arrays
+Like many other popular programming languages, strings in Python are arrays of bytes representing unicode characters.
+
+However, Python does not have a character data type, a single character is simply a string with a length of 1.
+
+Square brackets can be used to access elements of the string.
+"""
+c = "Hello_World"
+print(c[2])
