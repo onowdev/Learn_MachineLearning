b0a214a8b4120f75a850723979ee512b3ab18d49
onowdev <onowdev@gmail.com>
2022-07-25 16:59:58 +0700

    Statements

---
+++ Basic/01Variabel/2Statement.py
@@ -0,0 +1,9 @@
+"""
+Statements
+A statement is a unit of code that the Python interpreter can execute. We have
+seen two kinds of statements: print being an expression statement and assignment.
+"""
+
+print(9)
+x= 8
+print(x)
