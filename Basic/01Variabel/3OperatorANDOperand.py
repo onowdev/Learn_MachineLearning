c46483498518d34fa50320586ae85153db536aaa
onowdev <onowdev@gmail.com>
2022-07-25 17:06:42 +0700

    operan dan operan

---
+++ Basic/01Variabel/3OperatorANDOperand.py
@@ -0,0 +1,19 @@
+""" Operators and operands
+Operators are special symbols that represent computations like addition and mul-
+tiplication. The values the operator is applied to are called operands.
+The operators +, -, *, /, and ** perform addition, subtraction, multiplication,
+division, and exponentiation
+"""
+
+minute = 60
+print(minute / 90, "Second")
+
+a = 70
+b = 60
+
+print(a+b, "Subtraction")
+print(a-b, "Subtraction")
+print(a * b, "Perkalian")
+print(a/b, "Division")
+print(a%b, "Mudullo,")
+print(a ** 3, "Exponential")
