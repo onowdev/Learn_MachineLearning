f336e46c69428f2ec957ab284f7d07b23c0c03d2
onowdev <onowdev@gmail.com>
2022-07-25 16:27:50 +0700

    value and type

---
+++ Basic/01Variabel/1valuesandtype.py
@@ -0,0 +1,14 @@
+"""
+Value atau nilai adalah ssalah satu dasar dalam pemrograman 
+python, nilai pada python bisa berupa, angka, huruf. 1,2 dan "Hello, World!"
+merupakan contoh dari value
+
+Type merupakan jenis dari value, bisa berupa integer, string, float, atau boolean
+"""
+
+print(4) # menghasilkan output bernilai 4
+print("Hello, World") # menghasilkan Keluaran berupa Kata Hello,World
+print(9.8) # menghasilkan output bernilai 9.8 
+print(type(4))
+type("Hello,World")
+type(9.8)
