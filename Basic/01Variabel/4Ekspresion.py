4d6b9726c058bb5de5057fd71fda9121595b9cff
onowdev <onowdev@gmail.com>
2022-07-26 10:55:08 +0700

    espressioin

---
+++ Basic/01Variabel/4Ekspresion.py
@@ -0,0 +1,7 @@
+"""
+An expression is a combination of values, variables, and operators. A value all by
+itself is considered an expression, and so is a variable, so the following are all legal
+expressions (assuming that the variable x has been assigned a value):
+"""
+x = 9
+print(x + 1)
