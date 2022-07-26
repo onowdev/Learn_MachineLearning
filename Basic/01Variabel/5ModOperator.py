b2b33bbaea648c33606cca963d6876159c5b91ea
onowdev <onowdev@gmail.com>
2022-07-26 10:57:47 +0700

    modulo

---
+++ Basic/01Variabel/5ModOperator.py
@@ -0,0 +1,8 @@
+"""
+The modulus operator works on integers and yields the remainder when the first
+operand is divided by the second. In Python, the modulus operator is a percent
+sign (%). The syntax is the same as for other operators:
+
+"""
+quotient = 7//4
+print(quotient)
