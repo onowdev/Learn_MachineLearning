f03ebb678cdaf21eaa71389c8b9dbeada2726a35
onowdev <onowdev@gmail.com>
2022-08-03 09:11:19 +0700

    create var in py

---
+++ Basic/01Variabel/pyVariable/CreateVariable.py
@@ -0,0 +1,24 @@
+"""
+Creating Variables
+Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it."""
+
+x = 5 # Variabel x bertipe Integer
+y = "Johnny" #Variabel y bertipe String
+
+print(x)
+print(type(x)) # Cek tipe Variabel
+print(y)
+print(type(y)) # Cek Tipe Variabel
+
+# CASTING , Cara untuk menentukan tipe data dengan spesifik
+x = str(3)
+y = int(9)
+z = float(9)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
