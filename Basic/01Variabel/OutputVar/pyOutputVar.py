74300d50eaecd9d88e11579b6d8807a22368a4dd
onowdev <onowdev@gmail.com>
2022-08-03 09:39:43 +0700

    py output Variable

---
+++ Basic/01Variabel/OutputVar/pyOutputVar.py
@@ -0,0 +1,31 @@
+"""
+Output Variables
+The Python print() function is often used to output variables.
+"""
+x = "Python is Awesome"
+print(x)
+
+# In the print() function, you output multiple variables, separated by a comma:
+x = "python"
+y = "called"
+z = "programming language"
+print(x,y,z)
+
+# You can also use the + operator to output multiple variables:
+
+a = "Python"
+b = "Adalah salah satu"
+c = "Bahasa Pemrograman"
+print(a+ b+ c)
+
+# For numbers, the + character works as a mathematical operator:
+x = 9
+y = 8
+print(x + y)
+
+# In the print() function, when you try to combine
+# a string and a number with the + operator, Python will give you an error:
+
+x = "John"
+y = 90
+print(x , y)
