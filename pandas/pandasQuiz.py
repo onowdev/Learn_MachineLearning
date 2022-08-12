076aa0ae15be3d5072914b49c288f67c9bff5a7f
onowdev <onowdev@gmail.com>
2022-08-12 10:46:00 +0700

    py function

---
+++ pandas/pandasQuiz.py
@@ -0,0 +1,15 @@
+import pandas as pd
+df1 = pd.dataframe({
+    'key'  : ['k1','k2','k3','k4','k5'],
+    'val1' : [200,500,0,500,100],
+    'val2' : [30,50,100,20,10],
+})
+
+df2 = pd.dataframe({
+    'key'  : ['k1','k2','k5','k7','k10'],
+    'val3' : [1,2,3,4,5],
+    'val4' : [6,7,8,8,10]
+})
+gabung = pd.merge(df1,df2, validate="1:1")
+
+print(gabung)
